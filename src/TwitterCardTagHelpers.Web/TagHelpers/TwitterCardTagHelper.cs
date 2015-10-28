c5986ee190916e6ade0cebc730d6569086ed1179
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2015-10-28 22:38:33 +0100

    Switch to single-project development
    The dnx-watch does not pick changes in linked project
    in multip-project solution - hence the change. The resulting
    tag helper will be available in separate class library - but
    for now it is developed in .Web project

---
+++ src/TwitterCardTagHelpers.Web/TagHelpers/TwitterCardTagHelper.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Razor.Runtime.TagHelpers;
+
+namespace TwitterCardTagHelpers.Web.TagHelpers
+{
+    // You may need to install the Microsoft.AspNet.Razor.Runtime package into your project
+    [HtmlTargetElement("twitter-card")]
+    public class TwitterCardTagHelper : TagHelper
+    {
+        [HtmlAttributeName("type")]
+        public string type { get; set; }
+
+        [HtmlAttributeName("site")]
+        public string site { get; set; }
+
+        [HtmlAttributeName("title")]
+        public string title { get; set; }
+
+        [HtmlAttributeName("description")]
+        public string description { get; set; }
+
+        [HtmlAttributeName("image")]
+        public string image { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";
+        }
+    }
+}
