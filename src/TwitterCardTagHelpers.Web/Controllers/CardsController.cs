010429d7e1859636ce4de9971cdcce661744bf6d
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2015-11-10 23:27:48 +0100

    Add view/controller for Player card
    - view/action in controller
    - update navigation
    - example card documentation content

--- src/TwitterCardTagHelpers.Web/Controllers/CardsController.cs
+++ src/TwitterCardTagHelpers.Web/Controllers/CardsController.cs
@@ -23,5 +23,10 @@ namespace TwitterCardTagHelpers.Web.Controllers
         {
             return View();
         }
+
+        public IActionResult Player()
+        {
+            return View();
+        }
     }
 }
