78cfe23bc28b5244c192e7e526758c916e867272
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2015-11-30 22:28:58 +0100

    Scaffold empty test project

---
+++ src/TwitterCardTagHelpers.Tests/SampleTest.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace TwitterCardTagHelpers.Tests
+{
+    // see example explanation on xUnit.net website:
+    // https://xunit.github.io/docs/getting-started-dnx.html
+    public class SampleTest
+    {
+        [Fact(DisplayName = "It should pass")]
+        public void PassingTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
